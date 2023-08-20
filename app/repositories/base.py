46253182fa0b5182ce614d2553a294da36330dc9
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-20 23:54:19 +0500

    modifying queries

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -41,10 +41,11 @@ class BaseRepository:
 
     async def get_by_id(self, id: int) -> dict:
         stmt = select(self.model).where(self.model.id == id)
-        async with self.session.begin():
-            result = await self.session.execute(stmt)
-            print(result)
-        return result.scalars().one_or_none()
+        return await self.session.scalar(stmt)
+        # async with self.session.begin():
+        #     result = await self.session.execute(stmt)
+        #     print(result)
+        # return result.scalars().one_or_none()
 
     async def get_by_email(self, email: str) -> dict:
         stmt = select(self.model).where(self.model.email == email)
