754b4d4b5b2c414446378ad861967874f7a4c11e
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-20 15:45:40 +0500

    authentication

--- app/services/users.py
+++ app/services/users.py
@@ -51,8 +51,8 @@ class UsersService:
 
         access_token_expires = timedelta(minutes=JWTHandler.ACCESS_TOKEN_EXPIRE_MINUTES)
 
-        access_token = await self.users_repo.create_access_token(
-            data={"email": user.email}, expires_delta=access_token_expires
+        access_token = await JWTHandler.create_access_token(
+            data={"id": user.id, "email": user.email}, expires_delta=access_token_expires
         )
         return TokenSchema(access_token=access_token, token_type="Bearer")
 
