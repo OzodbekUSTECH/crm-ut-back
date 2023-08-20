aea75261e0939b2cd20459111ecbfed46f46dcec
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-21 01:37:42 +0500

    modifying queries

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -28,24 +28,28 @@ class BaseRepository:
 
     async def create_one(self, data: dict) -> dict:
         stmt = insert(self.model).values(**data).returning(self.model)
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
+        async with self.session.begin():
+            result = await self.session.execute(stmt)
+        return result.scalars().one()
 
     async def get_all(self, pagination: Pagination) -> list:
         stmt = select(self.model)
         stmt = stmt.order_by(self.model.id).offset(pagination.offset).limit(pagination.limit)
-        result = await self.session.execute(stmt)
+        async with self.session.begin():
+            result = await self.session.execute(stmt)
         return result.scalars().all()
 
     async def get_by_id(self, id: int) -> dict:
         stmt = select(self.model).where(self.model.id == id)
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
+        async with self.session.begin():
+            result = await self.session.execute(stmt)
+        return result.scalars().one_or_none()
 
     async def get_by_email(self, email: str) -> dict:
         stmt = select(self.model).where(self.model.email == email)
-        result = await self.session.execute(stmt)
-        return result.scalar_one_or_none()
+        async with self.session.begin():
+            result = await self.session.execute(stmt)
+        return result.scalars().one_or_none()
 
     async def update_one(self, id: int, data: dict) -> dict:
         stmt = update(self.model).where(self.model.id == id).values(**data).returning(self.model)
