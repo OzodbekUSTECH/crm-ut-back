bbd845e4d31146a05fb4251722643585e16ba960
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-21 17:13:53 +0500

    medifying repo and UOW

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -39,7 +39,7 @@ class BaseRepository:
     async def get_by_id(self, id: int) -> dict:
         stmt = select(self.model).where(self.model.id == id)
         result = await self.session.execute(stmt)
-        return result.first()
+        return result.scalar_one_or_none()
 
     async def get_by_email(self, email: str) -> dict:
         stmt = select(self.model).where(self.model.email == email)
