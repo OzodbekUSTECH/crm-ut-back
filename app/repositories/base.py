9b1d20554267a0b7348c6cb95d20b805e970ae11
OzodbekUSTECH <126599107+OzodbekUSTECH@users.noreply.github.com>
2023-08-21 15:28:17 +0500

    modifying code of UOW

--- app/repositories/base.py
+++ app/repositories/base.py
@@ -44,7 +44,7 @@ class BaseRepository:
     async def get_by_email(self, email: str) -> dict:
         stmt = select(self.model).where(self.model.email == email)
         result = await self.session.execute(stmt)
-        return result.scalar_one_or_none().to_read_model()
+        return result.scalar_one_or_none()
 
     async def update_one(self, id: int, data: dict) -> dict:
         stmt = update(self.model).where(self.model.id == id).values(**data).returning(self.model)
